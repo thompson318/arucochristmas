c5dea8d2aaafd50865254766222c374250026480
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-12-19 10:26:07 +0000

    Fixed typos

--- main.py
+++ main.py
@@ -11,11 +11,12 @@ def get_marker_pos():
     tracker.start_tracking()
 
     while True:
-        frame = get_frame()
-        if len(frame[3] > 0):
+        frame = tracker.get_frame()
+        if len(frame[3]) > 0:
             print ("\nx = ", np.mean(np.array(frame[3]).flat[3::16])) 
-            print ("\ny = ", np.mean(np.array(frame[3]).flat[7::16])) 
-
+            print ("y = ", np.mean(np.array(frame[3]).flat[7::16])) 
+        else:
+            print('.',end='')
 
 if __name__ == '__main__':
     get_marker_pos()
