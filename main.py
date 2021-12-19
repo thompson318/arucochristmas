871de41369449fb8b5547d69dc4c7dec4ae13b74
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-12-19 19:23:21 +0000

    Added tree

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gpiozero import LEDBoard
 from gpiozero.tools import random_values
 from sksurgeryarucotracker.arucotracker import ArUcoTracker
 
-def get_marker_pos():
+def get_marker_pos(tree):
     config = { "video source" : 0, 
                "debug" : False, 
                "aruco dictionary" : "DICT_ARUCO_ORIGINAL"
@@ -18,16 +18,16 @@ def get_marker_pos():
             x = np.mean(np.array(frame[3]).flat[3::16])
             print ("\nx = ", x) 
             print ("y = ", np.mean(np.array(frame[3]).flat[7::16])) 
-            light_led(int[x]%27)
-            delight_led(int[x]%27 + 1)
+            light_led(tree, int[x]%27)
+            delight_led(tree, int[x]%27 + 1)
         else:
             print('.',end='')
 
 
-def light_led(led_no):
+def light_led(tree, led_no):
     tree.led[led_no].on()
 
-def delight_led(led_no):
+def delight_led(tree, led_no):
     tree.led[led_no].on()
 
 if __name__ == '__main__':
