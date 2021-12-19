5c75702116f1d19ab453dd3cf4ff258df0e1086e
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-12-19 19:21:44 +0000

    Added some led code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from gpiozero import LEDBoard
+from gpiozero.tools import random_values
 from sksurgeryarucotracker.arucotracker import ArUcoTracker
 
 def get_marker_pos():
@@ -13,11 +15,23 @@ def get_marker_pos():
     while True:
         frame = tracker.get_frame()
         if len(frame[3]) > 0:
-            print ("\nx = ", np.mean(np.array(frame[3]).flat[3::16])) 
+            x = np.mean(np.array(frame[3]).flat[3::16])
+            print ("\nx = ", x) 
             print ("y = ", np.mean(np.array(frame[3]).flat[7::16])) 
+            light_led(int[x]%27)
+            delight_led(int[x]%27 + 1)
         else:
             print('.',end='')
 
+
+def light_led(led_no):
+    tree.led[led_no].on()
+
+def delight_led(led_no):
+    tree.led[led_no].on()
+
 if __name__ == '__main__':
-    get_marker_pos()
+
+    tree=LEDBoard(*range(2,28), pwm=False)    
+    get_marker_pos(tree)
 
