9ea3af8983fff4bf83d10ecf168d338e9344bc72
Stephen Thompson <s.thompson@ucl.ac.uk>
2021-12-22 16:48:35 +0000

    Fixes syntax error

--- arucochristmas/christmas.py
+++ arucochristmas/christmas.py
@@ -7,12 +7,16 @@ def initialise_tree():
     """Return a led board object with the
     26 leds number 2 to 27
     """
-    return tree=LEDBoard(*range(2,28), pwm=False)
+    return LEDBoard(*range(2,28), pwm=False)
 
 def light_led(tree, led_no):
+    """light the led"""
     print("lighting", led_no)
     tree.leds[led_no].on()
 
 def delight_led(tree, led_no):
+    """delight the led"""
+    if led_no > 25:
+        led_no -= 26
     print("de-lighting", led_no)
     tree.leds[led_no].off()
